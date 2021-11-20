e6ad98907d3f5ba8f1c457824fafdfeed277925a
cristpsantos <pontescr@gmail.com>
2021-11-19 21:53:20 -0300

    Create client and configure H2

---
+++ src/main/java/com/pontescr/projectbootcamp/ProjectbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.pontescr.projectbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pontescr/projectbootcamp/ProjectbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontescr.projectbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
